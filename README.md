a6777958878ae489950b10dbcab1bc5d862be71f
reconrez <reconrez@gmail.com>
2024-07-13 19:55:41 +0000

    theming testing

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# GATE
+# G.A.T.E. Galactic Archive & Tracking Engine
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 18.0.5.
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,6 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'GATE';
+  title = 'G.A.T.E.';
+  subTitle = 'Galactic Archive Tracking Engine'
 }
